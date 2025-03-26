0541092185a01f64b583c026e99a2f215a8cb3b1
incessmikee <incess.mikee@gmail.com>
2025-03-26 23:35:34 +0800

    prog 01: ask for user's input, find the first non-space charac

--- prog_01_print_without_lstrip_function.py
+++ prog_01_print_without_lstrip_function.py
@@ -1,6 +1,12 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 
 # ask for user's input
+user_input = input("Enter a word with leading spaces: ")
+
 # find the first non-space character
+index = 0
+while index < len(user_input) and user_input[index] == ' ':
+    index +=1
+
 # slice the string from the first non-space character
 # print
