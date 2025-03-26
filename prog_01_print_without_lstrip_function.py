f1e09c31776fdbad85d23d5261a79a2bdfc98d96
incessmikee <incess.mikee@gmail.com>
2025-03-26 23:30:32 +0800

    prog 01: psuedocode

---
+++ prog_01_print_without_lstrip_function.py
@@ -0,0 +1,6 @@
+# Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+# ask for user's input
+# find the first non-space character
+# slice the string from the first non-space character
+# print
