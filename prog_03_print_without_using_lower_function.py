095d13f7fd726328b390d35728ee12144c67f665
incessmikee <incess.mikee@gmail.com>
2025-03-28 13:06:08 +0800

    prog 03: pseudocode

---
+++ prog_03_print_without_using_lower_function.py
@@ -0,0 +1,7 @@
+# Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+# ask for user's input
+# check if the character is uppercase
+# convert to lowercase
+# don't change the non-uppercase
+# print result
