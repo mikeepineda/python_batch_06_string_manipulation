d2e5a76eca9de2b40109af6d78f0b9b3df3d3c06
incessmikee <incess.mikee@gmail.com>
2025-03-28 23:57:21 +0800

    prog 07: ask user input, calculate the total spaces

--- prog_07_print_without_using_center_function.py
+++ prog_07_print_without_using_center_function.py
@@ -1,6 +1,17 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 
 # ask for user's input
+user_input = input("Enter a word: ")
+
 # ask for desired width
+width = int(input("Enter the total width: "))
+
 # calculate the space for centering 
+if len(user_input) < width: 
+    total_spaces = width - len(user_input)
+    left_spaces = total_spaces // 2
+    right_spaces = total_spaces - left_spaces
+    user_input = " " * left_spaces + user_input + " " * right_spaces
+
 # print display
+print("Result:", user_input)
