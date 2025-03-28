84a1e04c1a28f6e53ac88c5c37a3204b01f960fe
incessmikee <incess.mikee@gmail.com>
2025-03-28 23:16:33 +0800

    prog 04: check if input is lowercase

--- prog_04_print_wiithout_using_isupper_function.py
+++ prog_04_print_wiithout_using_isupper_function.py
@@ -1,7 +1,16 @@
 # Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 
 # ask for user's input
+user_input = input("Enter a word: ")
+
 # check if the input is in lowercase, return false
+def custom_isupper(user_input):
+    for char in user_input:
+        if char in "abcdefghijklmnopqrstuvwxyz":
+            return False
 # else return true
+    return True
+
 # check if the input is uppercase
 # display result
+  
