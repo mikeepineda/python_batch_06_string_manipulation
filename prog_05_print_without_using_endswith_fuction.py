abd2317b971e2d33c9f0f4ede00e0dd1c51f4ba5
incessmikee <incess.mikee@gmail.com>
2025-03-28 23:28:37 +0800

    prog 05: pseudocode

---
+++ prog_05_print_without_using_endswith_fuction.py
@@ -0,0 +1,9 @@
+# Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+# ask for user's input
+# ask for the suffix 
+# check if the end of text matches the suffix
+# extract the last part of the input using slicing
+# compare with suffix
+# store the result
+# print display
