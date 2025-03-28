7c82de2749544ba2f5c3547d12d5783ddb1d0e70
incessmikee <incess.mikee@gmail.com>
2025-03-29 00:09:47 +0800

    prog 08: ask for input, add loop

--- prog_08_print_without_using_swapcase_function.py
+++ prog_08_print_without_using_swapcase_function.py
@@ -1,7 +1,11 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
 # ask for user input
+user_input = input("Enter a word: ")
+result = ""
+
 # loop every char
+for char in user_input: 
 # if char lowercase, subract 32 to become uppercase
 # if char uppercase, add 32 to become lowercase
 # if not letter, keep
