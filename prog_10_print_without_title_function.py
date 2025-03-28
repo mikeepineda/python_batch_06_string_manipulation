8802c9e3eb666275fb7703fe70434de82ecde4ce
incessmikee <incess.mikee@gmail.com>
2025-03-29 00:37:25 +0800

    prog 10: ask for user input and slice them

--- prog_10_print_without_title_function.py
+++ prog_10_print_without_title_function.py
@@ -1,7 +1,12 @@
 # Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 
 # ask for user input
+user_input = input("Enter a word: ")
+
 # split input into words
+words = text.split()
+result = ""
+
 # loop each word
 # capitalize first letter
 # rest to lowercase
