432e8a84da0cb1c4b03a720fbc2dd7701a41128f
incessmikee <incess.mikee@gmail.com>
2025-03-28 23:48:48 +0800

    prog 06: ask for input and width, check if string is shorter than the width, add spaces, print display: code complete

--- prog_06_print_without_using_ljust_function.py
+++ prog_06_print_without_using_ljust_function.py
@@ -1,7 +1,14 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
 # ask for user's input
-# ask for the desired width
-# add spaces at the end to match the given width
-# print display
+user_input = input("Enter a word: ")
 
+# ask for the desired width
+width = int(input("Enter the total width: "))
+
+# add spaces at the end to match the given width
+if len(user_input) < width: 
+    user_input = user_input + " " * (width - len(user_input))
+
+# print display
+print("Result: ", user_input)
