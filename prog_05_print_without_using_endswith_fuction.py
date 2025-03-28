5add198153875bac358346bc42e7d3ab2116b5d7
incessmikee <incess.mikee@gmail.com>
2025-03-28 23:31:48 +0800

    prog 05: slice the text, store result, code complete

--- prog_05_print_without_using_endswith_fuction.py
+++ prog_05_print_without_using_endswith_fuction.py
@@ -1,9 +1,16 @@
 # Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 
 # ask for user's input
+user_input = input("Enter a word: ")
+
 # ask for the suffix 
+suffix = input("Enter your suffix: ")
+
 # check if the end of text matches the suffix
 # extract the last part of the input using slicing
 # compare with suffix
 # store the result
+result = text[-len(suffix):] == suffix
+
 # print display
+print("Ends with suffix", result)
