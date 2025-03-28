602ee02c7e668e8909fdd53ed7d6970ef06b53cf
incessmikee <incess.mikee@gmail.com>
2025-03-28 13:16:06 +0800

    prog 03: convert input using ORD, keep non-uppercase, display result, code complete

--- prog_03_print_without_using_lower_function.py
+++ prog_03_print_without_using_lower_function.py
@@ -1,7 +1,16 @@
 # Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 
 # ask for user's input
-# check if the character is uppercase
-# convert to lowercase
-# don't change the non-uppercase
+user_input = input("Input a word: ")
+
+def to_lowercase(user_input):
+    result = ""
+    for char in user_input:
+        if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ": # check if the character is uppercase
+            result += chr(ord(char) + 32) # convert to lowercase using ORD
+        else: 
+            result += char # don't change the non-uppercase
+    return result
+
 # print result
+print(to_lowercase(user_input))
