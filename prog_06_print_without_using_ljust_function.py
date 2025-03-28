517b43c978d9f8b1c20c8740969b6473510acc8a
incessmikee <incess.mikee@gmail.com>
2025-03-28 23:40:58 +0800

    prog 06: pseudocode

---
+++ prog_06_print_without_using_ljust_function.py
@@ -0,0 +1,7 @@
+# Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+# ask for user's input
+# ask for the desired width
+# add spaces at the end to match the given width
+# print display
+
