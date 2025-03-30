5ed547d1b8806d2076e3da9b75c9ade1b2d78df4
incessmikee <incess.mikee@gmail.com>
2025-03-30 23:55:53 +0800

    prog 01: pseudocode

---
+++ batch_07/prog_01_print_without_lstrip_function.py
@@ -0,0 +1,6 @@
+# Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+# ask for user's input
+# initialize i to zero
+# create loop
+# create display
