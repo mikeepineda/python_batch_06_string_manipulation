cdcb9e60e3d4d0b74d918561c5e549261bfff8cd
incessmikee <incess.mikee@gmail.com>
2025-04-01 02:43:30 +0800

    prog 10: pseudocode

---
+++ batch_07/prog_10_print_without_using_rindex_function.py
@@ -0,0 +1,7 @@
+# Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+
+# ask for user's input 
+# ask for substring
+# loop backward
+# return -1 if not found
+# print
