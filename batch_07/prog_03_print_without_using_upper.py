37d0b2f5d97cdf969944c69b9b98fd9b48527913
incessmikee <incess.mikee@gmail.com>
2025-04-01 01:01:26 +0800

    prog 03: pseudocode

---
+++ batch_07/prog_03_print_without_using_upper.py
@@ -0,0 +1,7 @@
+# Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
+# ask for user's input
+# check if the character is lowercase
+# convert to uppercase using ORD
+# don't change the non-lowercase
+# print result
