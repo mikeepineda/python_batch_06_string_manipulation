c9e463106c22ca8a32637c1ac68c2af3d34f2f28
incessmikee <incess.mikee@gmail.com>
2025-04-01 02:18:22 +0800

    prog 07: pseudocode

---
+++ batch_07/prog_07_print_without_using_zfill.py
@@ -0,0 +1,6 @@
+# Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+
+# ask for user's input
+# ask for desired width
+# calculate how many zeros to add
+# print
