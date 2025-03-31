9ab57d865e117fe46c86f928ebfe90dda42debcb
incessmikee <incess.mikee@gmail.com>
2025-04-01 01:43:44 +0800

    prog 05: pseudocode

---
+++ batch_07/prog_05_print_without_startswith_function.py
@@ -0,0 +1,7 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+# ask for user's input
+# ask for the prefix
+# extract the first part of the input using slicing
+# compare with prefix
+# print display
