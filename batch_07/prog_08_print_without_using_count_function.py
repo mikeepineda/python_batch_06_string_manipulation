d7fb3d5a31d41dd30574af3360a4f381f548c119
incessmikee <incess.mikee@gmail.com>
2025-04-01 02:28:45 +0800

    prog 08: ask user input, substring, def

--- batch_07/prog_08_print_without_using_count_function.py
+++ batch_07/prog_08_print_without_using_count_function.py
@@ -1,7 +1,15 @@
 # Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
 # ask for user input
+user_input = input("Enter a string: ")
 # ask for substring
+substring = input("Enter the substring to count: ")
+
 # def custom count
+def custom_count(string, substring):
+    count = 0
+    index = 0
+
 # create loop
+
 # print
