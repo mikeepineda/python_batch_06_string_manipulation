29f51c19a2eb9fa3066107b92ff47271ddb3d0f9
incessmikee <incess.mikee@gmail.com>
2025-04-01 02:46:06 +0800

    prog 10: ask user input and substring, loop backwards

--- batch_07/prog_10_print_without_using_rindex_function.py
+++ batch_07/prog_10_print_without_using_rindex_function.py
@@ -1,7 +1,12 @@
 # Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 
-# ask for user's input 
-# ask for substring
-# loop backward
-# return -1 if not found
+user_input = input("Enter a string: ")  # ask for user's input 
+substring = input("Enter the substring to find: ")  # ask for substring
+
+def custom_rindex(string, substring):
+    for i in range(len(string) - len(substring), -1, -1):  #loop backward
+        if string[i:i + len(substring)] == substring: 
+            return i  # return the last found index
+    return -1  # return -1 if not found
+
 # print
