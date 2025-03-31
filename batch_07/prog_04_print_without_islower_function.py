1c99a4fe944b4f6f2a087d9fc293d4ec038f5191
incessmikee <incess.mikee@gmail.com>
2025-04-01 01:19:07 +0800

    prog 04: ask user input, return false if input is uppercase otherwise return true

--- batch_07/prog_04_print_without_islower_function.py
+++ batch_07/prog_04_print_without_islower_function.py
@@ -1,7 +1,15 @@
 # Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
 
 # ask for user's input
+user_input = input("Enter a word: ")
+
 # check if the input is in uppercase, return false
+def custom_islower(user_input):
+    for char in user_input:
+        if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            return False
 # else return true
+    return True
+
 # check if the input is uppercase
 # display result
