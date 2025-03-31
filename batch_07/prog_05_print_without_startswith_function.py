642ad0c60c2119925ae3a961f1ce680d67f7eee2
incessmikee <incess.mikee@gmail.com>
2025-04-01 01:45:27 +0800

    prog 05: ask user input, slice, print: code complete

--- batch_07/prog_05_print_without_startswith_function.py
+++ batch_07/prog_05_print_without_startswith_function.py
@@ -1,7 +1,14 @@
 # Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
 # ask for user's input
+user_input = input("Enter a word: ")
+
 # ask for the prefix
+prefix = input("Enter your prefix: ")
+
 # extract the first part of the input using slicing
 # compare with prefix
-# print display
+if user_input[:len(prefix)] == prefix:
+    print("The string starts with the given prefix.")   # print display
+else:
+    print("The string does NOT start with the given prefix.")
