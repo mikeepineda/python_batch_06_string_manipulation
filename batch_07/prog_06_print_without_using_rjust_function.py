61ce42ebbfa90c59a80efa9ba91ae43d60b0e05d
incessmikee <incess.mikee@gmail.com>
2025-04-01 02:13:19 +0800

    prog 06: pseudocode

---
+++ batch_07/prog_06_print_without_using_rjust_function.py
@@ -0,0 +1,6 @@
+# Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
+# ask for user's input
+# ask for the desired width
+# add spaces at the start to match the given width
+# print display
