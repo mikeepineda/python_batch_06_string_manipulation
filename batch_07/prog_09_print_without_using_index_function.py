4a91cc36ed67f36f1162fb9dc5cd1a7ff16fcdf3
incessmikee <incess.mikee@gmail.com>
2025-04-01 02:35:55 +0800

    prog 09: pseudocode

---
+++ batch_07/prog_09_print_without_using_index_function.py
@@ -0,0 +1,7 @@
+# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
+# ask for user's input 
+# ask for substring
+# def cutom index
+# create loop
+# print
