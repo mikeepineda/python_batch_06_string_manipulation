3314621b074fa62f96cda08a479ce6c727ac6a4c
incessmikee <incess.mikee@gmail.com>
2025-04-01 02:26:24 +0800

    prog 08: pseudocode

---
+++ batch_07/prog_08_print_without_using_count_function.py
@@ -0,0 +1,7 @@
+# Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+# ask for user input
+# ask for substring
+# def custom count
+# create loop
+# print
